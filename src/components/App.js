7bfb0dff4a9e8e5028686dd8446c1d1bf55579cb
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-03-18 22:28:07 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './../styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import UsersPage from './components/UsersPage';
+import NotificationsPage from './components/NotificationsPage';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/users" component={UsersPage} />
+        <Route path="/notifications" component={NotificationsPage} />
+        <Route path="/create-post" component={CreatePost} />
+        <Route path="/edit-post/:id" component={EditPost} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
