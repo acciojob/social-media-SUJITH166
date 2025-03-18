c5d7b8effa98723bf87055a7fe0e20bc84886616
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-03-18 15:32:32 +0530

    Navbar.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-
 import React from "react";
-import './../styles/App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const App = () => {
+import {
+  fetchNotifications,
+  selectAllNotifications
+} from "../features/notifications/notificationsSlice";
+
+export const Navbar = () => {
+  const dispatch = useDispatch();
+  const notifications = useSelector(selectAllNotifications);
+  const numUnreadNotifications = notifications.filter((n) => !n.read).length;
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications());
+  };
+
+  let unreadNotificationsBadge;
+
+  if (numUnreadNotifications > 0) {
+    unreadNotificationsBadge = (
+      <span className="badge">{numUnreadNotifications}</span>
+    );
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <nav>
+      <section>
+        <h1>GenZ</h1>
 
-export default App
+        <div className="navContent">
+          <div className="navLinks">
+            <Link to="/">Posts</Link>
+            <Link to="/users">Users</Link>
+            <Link to="/notifications">
+              Notifications {unreadNotificationsBadge}
+            </Link>
+          </div>
+
+          <button className="button" onClick={fetchNewNotifications}>
+            Refresh Notifications
+          </button>
+        </div>
+      </section>
+    </nav>
+  );
+};
